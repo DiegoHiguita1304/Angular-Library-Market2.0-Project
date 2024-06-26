1a04ee1380eaad35b0844dace37aa0ef167cf413
Diego Alexander Higuita Jaramillo <dahiguita@tcc.com.co>
2024-06-26 11:00:33 -0500

    Se implementa localStorge para la persistencia de los datos entre sesiones del navegador y behaviorSubject para emitir cambios en el estado del carrito en tiempo real a cualquier componente que este suscrito a este servicio

--- src/app/services/Cart.service.ts
+++ src/app/services/Cart.service.ts
@@ -1,4 +1,3 @@
-// src/app/services/cart.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BookView } from '../models/bookView.model';
@@ -8,23 +7,59 @@ import { Observable, BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class CartService {
-  // getItems(): any[] {
-  //   throw new Error('Method not implemented.');
-  // }
-  private items: BookView[] = []; // Array para almacenar los libros en el carrito
-  private cartSubject = new BehaviorSubject<BookView[]>([]); // BehaviorSubject para emitir cambios en el carrito
+  private storageKey = 'shoppingCart';
+  private items: BookView[] = [];
+  private cartSubject = new BehaviorSubject<BookView[]>(this.loadItems());
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    // Listener to save items to localStorage before the window is unloaded
+    window.addEventListener('beforeunload', () => this.saveItems());
+  }
+
+  private saveItems() {
+    localStorage.setItem(this.storageKey, JSON.stringify(this.items));
+  }
+
+  private loadItems(): BookView[] {
+    const savedItems = localStorage.getItem(this.storageKey);
+    this.items = savedItems ? JSON.parse(savedItems) : [];
+    return this.items;
+  }
 
   addToCart(book: BookView): void {
-    this.items.push(book); // Agregar un libro al carrito
+    const existingItem = this.items.find(item => item.id_libro === book.id_libro);
+    if (existingItem) {
+      existingItem.quantity++;
+    } else {
+      book.quantity = 1;
+      this.items.push(book);
+    }
     this.cartSubject.next(this.items); // Emitir el nuevo estado del carrito
-    console.log('Carrito actualizado:', this.items);
+    this.saveItems(); // Guardar en localStorage
   }
 
   removeFromCart(book: BookView): void {
-    this.items = this.items.filter(item => item.id_libro !== book.id_libro); // Eliminar un libro del carrito
+    this.items = this.items.filter(item => item.id_libro !== book.id_libro);
     this.cartSubject.next(this.items); // Emitir el nuevo estado del carrito
+    this.saveItems(); // Guardar en localStorage
+  }
+
+  increaseQuantity(book: BookView): void {
+    const item = this.items.find(item => item.id_libro === book.id_libro);
+    if (item) {
+      item.quantity++;
+      this.cartSubject.next(this.items); // Emitir el nuevo estado del carrito
+      this.saveItems(); // Guardar en localStorage
+    }
+  }
+
+  decreaseQuantity(book: BookView): void {
+    const item = this.items.find(item => item.id_libro === book.id_libro);
+    if (item && item.quantity > 1) {
+      item.quantity--;
+      this.cartSubject.next(this.items); // Emitir el nuevo estado del carrito
+      this.saveItems(); // Guardar en localStorage
+    }
   }
 
   getItems(): BookView[] {
@@ -32,11 +67,12 @@ export class CartService {
   }
 
   getCart(): Observable<BookView[]> {
-    return this.cartSubject.asObservable(); // Obtener una Observable del estado actual del carrito
+    return this.cartSubject.asObservable();
   }
 
   clearCart(): void {
-    this.items = []; // Vaciar el carrito
+    this.items = [];
     this.cartSubject.next(this.items); // Emitir el nuevo estado del carrito (carrito vacío)
+    this.saveItems(); // Guardar en localStorage
   }
 }
